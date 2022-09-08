97ee34f1b44b9e7fea21e285848534473d1aba5b
eddy-guo <eddyyanruguo@gmail.com>
2022-09-08 00:59:57 -0400

    event/emit for fe integration, .push change

--- backend/contract.sol
+++ backend/contract.sol
@@ -3,6 +3,9 @@
 pragma solidity ^0.8.0;
 
 contract duckFactory {
+
+    event NewDuck(uint duckId, string name, uint dna); // event declaration
+
     uint dnaValue = 16; // Duck DNA determined by 16 digit number
     uint dnaMod = 10**16; // used in future to mod (%) numbers down to 16 digits
 
@@ -18,8 +21,10 @@ contract duckFactory {
     // private function to create ducks and avoid other people creating ducks
     // _name stored in memory - if val changes in function, val of the original variable changes
     // parameter names start with underscore to differentiate from global variables (convention)
-    function _createDuck (string memory _name, uint _data) private {
-        ducks.push(Duck(_name, _data)); // add a new Duck(parameters) to the array
+    function _createDuck (string memory _name, uint _dna) private {
+        ducks.push(Duck(_name, _dna)); // add a new Duck to the array
+        uint id = ducks.length - 1; // store the ducks' id
+        emit NewDuck(id, _name, _dna); // emit event here, FE app can listen to emitted event
     }
 
     // private function that takes in a string to return a random dna number
@@ -29,6 +34,7 @@ contract duckFactory {
         return rand % dnaMod; // DNA defined as 16 digits only
     }
 
+    // public function that creates random duck - name generates unique dna, create duck!
     function _createRandomDuck (string memory _name) public {
         uint randomDna = _generateRandomDna(_name);
         _createDuck(_name, randomDna);
