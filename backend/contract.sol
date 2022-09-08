e5211df8bf091d12bd51fa63f378aee80e84abef
eddy-guo <eddyyanruguo@gmail.com>
2022-09-07 23:56:24 -0400

    generate dna and create duck functions

--- backend/contract.sol
+++ backend/contract.sol
@@ -15,11 +15,23 @@ contract duckFactory {
     // public dynamic array to store a raft of ducks :)
     Duck[] public ducks;
 
-    // public function to create ducks
+    // private function to create ducks and avoid other people creating ducks
     // _name stored in memory - if val changes in function, val of the original variable changes
     // parameter names start with underscore to differentiate from global variables (convention)
-    function createDuck (string memory _name, uint _data) public {
+    function _createDuck (string memory _name, uint _data) private {
         ducks.push(Duck(_name, _data)); // add a new Duck(parameters) to the array
     }
-    
+
+    // private function that takes in a string to return a random dna number
+    // view function since only viewing contract variables and not changing them
+    function _generateRandomDna (string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str))); // typecast as uint before storing
+        return rand % dnaMod; // DNA defined as 16 digits only
+    }
+
+    function _createRandomDuck (string memory _name) public {
+        uint randomDna = _generateRandomDna(_name);
+        _createDuck(_name, randomDna);
+    }
+
 }
