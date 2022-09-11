b07f3ff37e088a0a7e09a14437c5dbf34bdc20ba
eddy-guo <eddyyanruguo@gmail.com>
2022-09-10 21:25:27 -0400

    multiplying ducks function

--- backend/duckfeeding.sol
+++ backend/duckfeeding.sol
@@ -2,6 +2,14 @@
 
 pragma solidity ^0.8.0;
 
-contract DuckFeeding is duckFactory {
+import "./duckfactory.sol";
 
+contract DuckFeeding is duckFactory {
+    function feedAndMultiply (uint _duckId, uint _targetDna) public {
+        require(msg.sender == duckToOwner[_duckId]);
+        Duck storage myDuck = ducks[_duckId];
+        _targetDna = _targetDna % dnaMod;
+        uint newDna = (myDuck.dna + _targetDna) / 2;
+        _createDuck("tempName", newDna);
+    }
 }
